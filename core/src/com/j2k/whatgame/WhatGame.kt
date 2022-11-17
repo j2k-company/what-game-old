f9976a914626921807aeda26688c4bcefc81c779
jaka2005 <jakalogin1808@gmail.com>
2022-11-17 14:27:37 +0400

    initial commit

---
+++ core/src/com/j2k/whatgame/WhatGame.kt
@@ -0,0 +1,28 @@
+package com.j2k.whatgame
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.ScreenUtils
+
+class WhatGame : ApplicationAdapter() {
+    lateinit var batch: SpriteBatch
+    lateinit var img: Texture
+
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+    }
+
+    override fun render() {
+        ScreenUtils.clear(1f, 0f, 0f, 1f)
+        batch.begin()
+        batch.draw(img, 0f, 0f)
+        batch.end()
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
+}
---
+++ desktop/src/com/j2k/whatgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.j2k.whatgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.j2k.whatgame.WhatGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WhatGame(), config);
+	}
+}
